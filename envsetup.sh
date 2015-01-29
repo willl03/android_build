d4cb3034634d706200506f98d2a04aa2b868019b
willl03 <wgangers@gmail.com>
2015-01-28 23:25:02 -0500

    (2/2) W03ify build name

--- envsetup.sh
+++ envsetup.sh
@@ -720,7 +720,7 @@ function eat()
 {
     if [ "$OUT" ] ; then
         MODVERSION=$(get_build_var CM_VERSION)
-        ZIPFILE=cm-$MODVERSION.zip
+        ZIPFILE=$MODVERSION.zip
         ZIPPATH=$OUT/$ZIPFILE
         if [ ! -f $ZIPPATH ] ; then
             echo "Nothing to eat"
